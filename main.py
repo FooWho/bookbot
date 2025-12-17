7288d7b3809ee0e97be5f54e49cef344bc56568e
Jason Elison <jason.elison@gmail.com>
2025-12-16 23:43:29 -0500

    Hard coded book report done.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_characters
+from stats import sorted_list
 
 
 def get_book_text(book):
@@ -12,9 +13,17 @@ def main():
     book_text = get_book_text('books/frankenstein.txt')
     num_words = count_words(book_text)
     character_dict = count_characters(book_text)
+    result = sorted_list(character_dict)
 
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(character_dict)
+    print("--------- Character Count -------")
+    for item in result:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
     
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,19 @@ def count_characters(text):
     return char_count
 
 
+def sort_helper(element):
+    return element["num"]
+
+def sorted_list(char_count):
+    result_list = list()
+
+    for element in char_count:
+        result_list.append({"char": element, "num": char_count[element]})
+
+    result_list.sort(reverse=True, key=sort_helper)
+
+    return result_list
+
+
+    
+    
